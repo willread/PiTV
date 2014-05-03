07d810d23e756d884649efa00096a4969e23228c
Will Blanchette <will@unata.com>
2014-05-02 21:30:02 -0400

    Stuff

--- apps/launcher/static/js/app/app.js
+++ apps/launcher/static/js/app/app.js
@@ -4,6 +4,8 @@ function LauncherCtrl($scope, $http){
 	$http.get("/files").
 		success(function(files){
 			angular.foreach(files, function(file, i){
+				file = file.split("/");
+				file = file[file.length - 1];
 				$scope.files.push({path: file, index: i});
 			});
 		});
