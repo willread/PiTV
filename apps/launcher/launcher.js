3190e0cab924cc11c8f303c1fc7eb1bb8b388442
Will Blanchette <will@unata.com>
2014-05-02 21:37:34 -0400

    Changed exec to spawn

--- apps/launcher/launcher.js
+++ apps/launcher/launcher.js
@@ -50,7 +50,7 @@ var videoChild;
 app.put("/play", function(req, res){
 	console.log("playing " + req.body.id, files[req.body.id]);
 	videoChild.kill();
-	videoChild = exec("omxplayer -o hdmi \"" + files[req.body.id] + "\"");
+	videoChild = spawn("omxplayer" ["-o hdmi \"" + files[req.body.id] + "\""]);
 });
 
 // Start server
